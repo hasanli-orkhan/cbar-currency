c9eb66038df437a3e9cc47cd7558b31948c5e316
hasanli-orkhan <orkhan.hasanli.92@gmail.com>
2022-12-01 15:40:12 +0400

    docs: adding comments

--- src/main/java/info/md7/cbar_currency/model/Constants.java
+++ src/main/java/info/md7/cbar_currency/model/Constants.java
@@ -3,10 +3,20 @@ package info.md7.cbar_currency.model;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+/**
+ * Constants used in application
+ */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Constants {
 
+  /**
+   * CBAR Currencies Base URL
+   */
   public static final String BASE_URL = "https://www.cbar.az/currencies/";
+
+  /**
+   * Page Extension (xml)
+   */
   public static final String PAGE_EXTENSION = ".xml";
 
 }
